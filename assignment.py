542e8fc2af1398a1a30ae18e3e86874b9ec8e960
Rajinish-03 <rajnishrajput3103@gmail.com>
2024-09-20 21:43:22 +0530

    Python Bootcamp Test

---
+++ assignment.py
@@ -0,0 +1,17 @@
+CY=int(input("Enter the current year"))
+BY=int(input("Enter the born year"))
+age=CY-BY
+print("your age is:",age)
+
+
+weight=int(input("enter the weight"))
+height=float(input("enter the hight"))
+BMI=weight/height**2
+print(BMI)
+
+print("Convert rupees int dollers")
+rupeesAmount = int(input("Enter the amount in rupees:"))
+rsIntoDollor = (rupeesAmount / 84)
+print(rupeesAmount,"Convert into Dollor",rsIntoDollor);
+
+
